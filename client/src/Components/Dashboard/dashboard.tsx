452ffe891a3b54791b95f30100b573b1c918b8eb
aitak1 <149903267+aitak1@users.noreply.github.com>
2024-02-04 17:29:54 -0600

    fixed and added things to map

--- client/src/Components/Dashboard/dashboard.tsx
+++ client/src/Components/Dashboard/dashboard.tsx
@@ -1,46 +1,155 @@
-import React, { useState, useEffect } from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import React, { useState, useEffect, useCallback } from "react";
+import { Loader } from '@googlemaps/js-api-loader';
 import { Link } from 'react-router-dom';
 import "./dashboard.css"; 
 
+
+//testing marking 'garage sale' locations
+const locationsArray =[
+  "908 granview dr lewisville tx",
+  "2021 vista dr lewisville tx",
+  "1812 Blair oak drive lewisville tx",
+  "118 lynn avenue lewisville tx"
+];
+
+//add markers for garage sale locations within radius of user position
+const findTheWay = async (circle, map, userPosition) => {
+  for(const location of locationsArray){
+
+    try {
+      // Perform geocoding to convert address to coordinates using a geocoding service
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(location)}&key=AIzaSyBqSTtw4vop05TMAcAXcdClNgIKApvgYVU`
+      );
+  
+      if (response.ok) {
+        const data = await response.json();
+        if (data.results && data.results[0] && data.results[0].geometry) {
+          const { lat, lng } = data.results[0].geometry.location;
+          if(google.maps.geometry.spherical.computeDistanceBetween(
+            new google.maps.LatLng(lat, lng),
+            new google.maps.LatLng(userPosition.lat, userPosition.lng)
+          ) <= circle.getRadius())
+          {
+            new google.maps.Marker({
+              position: {lat , lng},
+              map,
+              title: location,
+              // Customize the marker properties or add other features as needed
+            });
+          }
+        } else {
+          console.log(data);
+          alert("Invalid location");
+        }
+      } else {
+        console.error("Geocoding request failed");
+      }
+    } catch (error) {
+      console.error("Error during geocoding:", error);
+    }
+  }
+    
+};
+
 function SearchLocation() {
-  const mapStyles = [
-    {
-      featureType: 'poi',
-      elementType: 'labels',
-      stylers: [
-        { visibility: 'off' } // Hide points of interest labels
-      ]
-    }];
     const [location, setLocation] = useState("");
     const [userPosition, setUserPosition] = useState({ lat: 33.253946, lng: -97.152896 });
     const [validAddress, setAddressValidity] = useState(false);
 
-    const handleSearch = async () => {
-      try {
-        // Perform geocoding to convert address to coordinates using a geocoding service
-        const response = await fetch(
-          `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(location)}&key=AIzaSyBqSTtw4vop05TMAcAXcdClNgIKApvgYVU`
-        );
+    const loadMap = useCallback(async ()=>{
+      const mapStyles = [
+        {
+          featureType: 'poi',
+          elementType: 'labels',
+          stylers: [
+            { visibility: 'off' } // Hide points of interest labels
+          ]
+        }];
+      const loader = new Loader({
+        apiKey: 'AIzaSyBqSTtw4vop05TMAcAXcdClNgIKApvgYVU', // Replace with your actual API key
+        version: 'weekly'});
+      
+      
+      loader.load().then(() => {
+        const google = window.google;
+        if (google) {
+          const map = new google.maps.Map(document.getElementById('map')!, {
+            center: userPosition || { lat: 33.253946, lng: -97.152896 },
+            zoom: userPosition ? 17 : 1,
+            styles: mapStyles
+          });
+
+          if (userPosition && validAddress) {
+            const image = "/assets/currentLocationMarker.PNG";
+        
+            const marker = new google.maps.Marker({
+              position: userPosition,
+              map,
+              title: 'Your Location',
+              icon: image
+            });
+            
+            const circle = new google.maps.Circle({
+              map,
+              center: userPosition,
+              radius: .5 * 1609.34, // 5 miles in meters
+              fillColor: '#4285F4', // Blue fill color
+              fillOpacity: 0.2, // Adjust the opacity as needed
+              strokeColor: '#4285F4', // Blue border color
+              strokeOpacity: 0.8, // Adjust the opacity as needed
+              strokeWeight: 2 // Border thickness
+            });
     
-        if (response.ok) {
-          const data = await response.json();
-          if (data.results && data.results[0] && data.results[0].geometry) {
-            const { lat, lng } = data.results[0].geometry.location;
-            setUserPosition({ lat, lng });
-            console.log("Its working!");
-            setAddressValidity(true);
+            // Adjust the map bounds to include the marker and circle
+            const bounds = new google.maps.LatLngBounds();
+            bounds.extend(marker.getPosition()!);
+            bounds.union(circle.getBounds()!);
+            map.fitBounds(bounds); 
+
+            findTheWay(circle, map, userPosition);
+          }
+         }
+      });
+    },[userPosition, validAddress]);
+
+    useEffect(() => {
+      loadMap();
+    }, [loadMap]);
+
+    const handleSearch = async () => {
+      if(location.trim() !==''){
+        try {
+          // Perform geocoding to convert address to coordinates using a geocoding service
+          const response = await fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(location)}&key=AIzaSyBqSTtw4vop05TMAcAXcdClNgIKApvgYVU`
+          );
+      
+          if (response.ok) {
+            const data = await response.json();
+            if (data.results && data.results[0] && data.results[0].geometry) {
+              const { lat, lng } = data.results[0].geometry.location;
+              setUserPosition({ lat, lng });
+              console.log("Its working!");
+              setAddressValidity(true);
+              loadMap();
+            } else {
+              console.log(data);
+              alert("Invalid location");
+              setAddressValidity(false);
+            }
           } else {
-            console.log(data);
-            alert("Invalid location");
+            console.error("Geocoding request failed");
             setAddressValidity(false);
           }
-        } else {
-          console.error("Geocoding request failed");
+        } catch (error) {
+          console.error("Error during geocoding:", error);
           setAddressValidity(false);
         }
-      } catch (error) {
-        console.error("Error during geocoding:", error);
+        setLocation("");
+      }
+      else{
+        alert("Please enter a location or use current location");
         setAddressValidity(false);
       }
     };
@@ -61,6 +170,8 @@ function SearchLocation() {
                 lng: position.coords.longitude
               });
               setAddressValidity(true);
+              loadMap();
+              console.log(userPosition, 'confusion');
             },
             (error) => {
               console.error('Error getting user location:', error);
@@ -76,23 +187,19 @@ function SearchLocation() {
       }
     };
 
-    useEffect(() => {
-
-      if (userPosition && userPosition.lat && userPosition.lng && validAddress) {
-        console.log('userPosition updated:', userPosition);
-      }
-    }, [userPosition, validAddress]);
-
   return (
     <div className="lower-content">
       <div className="search-map">
         <div className="input-container">
           <input
+            id="locationInput"
+            name="location"
             type="text"
             placeholder="Location..."
             value={location}
             onChange={(e) => setLocation(e.target.value)}
             onKeyDown={ handleEnterKey}
+            aria-label="Search Location"
           />
           <button type="button" className="searchButton" onClick={handleSearch}>
             Search
@@ -104,24 +211,7 @@ function SearchLocation() {
               />
 
         </div>
-        <div className="map">
-            <GoogleMap
-              mapContainerClassName="map"
-              center={userPosition || { lat:  33.253946, lng: -97.152896 }}
-              zoom={userPosition ? 17 : 1}
-              options={{styles: mapStyles}}
-            >
-              
-                {/* Conditionally render the Marker based on userPosition and validAddress */}
-            {userPosition && validAddress && (
-              <Marker
-                onLoad={(marker) => console.log('marker:', marker)}
-                position={userPosition}
-                title="Your Location"
-              />
-            )}
-              
-            </GoogleMap>
+        <div className="map" id="map">
         </div>
       </div>
     </div>
@@ -132,6 +222,7 @@ function SearchLocation() {
 function SavedSales() {
   return (
     <div className="saved">
+    <div className="sidebar-container">
     <div className="sidebar">
         <div className="name">
           Saved Sales
@@ -143,12 +234,14 @@ function SavedSales() {
         </ol>
     </div>
     </div>
+    </div>
   );
 }
 
 function ResultSales() {
   return (
     <div className="result">
+    <div className="sidebar-container">
     <div className="sidebar">
         <div className="name">
           Results
@@ -158,6 +251,7 @@ function ResultSales() {
           <li>Second</li>
           <li>Third</li>
         </ol>
+    </div>
     </div>
     </div>
   );
@@ -215,24 +309,9 @@ function Dashboard(){
       {/* before */}
       <div className="lower-content">
         {SavedSales()}
-        <LoadScript
-          googleMapsApiKey="AIzaSyBqSTtw4vop05TMAcAXcdClNgIKApvgYVU"
-        >
-          <SearchLocation />
-        </LoadScript>
+        {SearchLocation()}
         {ResultSales()}
       </div>
-
-      {/* after */}
-      {/* <div className="lower-content">
-        
-        <LoadScript
-          googleMapsApiKey="AIzaSyBqSTtw4vop05TMAcAXcdClNgIKApvgYVU"
-        >
-          <SearchLocation />
-          {resultSales()}
-        </LoadScript>
-      </div> */}
     </div>
   );
 }
